f95ee28b0bd35e3eefadbaf0d37b9ac8f30c7aeb
NicParsons <96993843+NicParsons@users.noreply.github.com>
2024-07-20 11:06:52 +1000

    Made Currency enumeration Codable

--- Sources/OBFoundation/Currency Exchange/Currency.swift
+++ Sources/OBFoundation/Currency Exchange/Currency.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-public enum Currency: String, CaseIterable, Identifiable {
+public enum Currency: String, CaseIterable, Identifiable, Codable {
 	// Cannot currently support EUR as there is a weird bug that I've posted about in the forums.
     case AUD, CAD, GBP, IDR, NZD, USD
     public var id: String { self.rawValue }
