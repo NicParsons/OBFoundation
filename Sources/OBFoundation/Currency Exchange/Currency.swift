568d4b90e3f78ccf4458684b2baa369bc1d18d63
NicParsons <96993843+NicParsons@users.noreply.github.com>
2024-08-25 19:07:44 +1000

    Make Currency Comparable

--- Sources/OBFoundation/Currency Exchange/Currency.swift
+++ Sources/OBFoundation/Currency Exchange/Currency.swift
@@ -1,9 +1,13 @@
 import Foundation
 
-public enum Currency: String, CaseIterable, Identifiable, Codable {
+public enum Currency: String, CaseIterable, Identifiable, Codable, Comparable {
 	// Cannot currently support EUR as there is a weird bug that I've posted about in the forums.
     case AUD, CAD, GBP, IDR, NZD, USD
     public var id: String { self.rawValue }
+
+	public static func <(lhs: Currency, rhs: Currency) -> Bool {
+		return lhs.rawValue < rhs.rawValue
+	}
 } // Currency enum
 
 //  Currency.swift
